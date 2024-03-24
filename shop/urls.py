1020f13a72426d47e7ec24781fdb3c516890d413
jane mckenna <janemckenna@hotmail.com>
2024-03-24 18:37:33 +0000

    Feat: added header

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', include('home.urls')),
+    path('', views.shop, name='shop'),
 ]
--- the_club/urls.py
+++ the_club/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('/', include('home.urls')),
+    path('', include('home.urls')),
     path('shop/', include('shop.urls')),
 ]
