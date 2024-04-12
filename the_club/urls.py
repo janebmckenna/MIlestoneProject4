02a622efecc06e846faeee73d8c28f1e76ad5933
jane mckenna <janemckenna@hotmail.com>
2024-04-12 16:16:48 +0000

    Feat: added checkout page

--- the_club/urls.py
+++ the_club/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('kit/', include('kit.urls')),
     path('bag/', include('bag.urls')),
     path('subs/', include('subs.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
