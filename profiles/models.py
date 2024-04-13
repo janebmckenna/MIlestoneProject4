c9ed063a863a5f93d262a38f3c05f2bf1206fc3f
jane mckenna <janemckenna@hotmail.com>
2024-04-13 16:53:40 +0000

    Feat: added profile page content

--- profiles/models.py
+++ profiles/models.py
@@ -12,7 +12,6 @@ class UserProfile(models.Model):
     delivery info and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=50, null=True, blank=True)
     default_email = models.EmailField(max_length=254, null=True, blank=True)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_house_number = models.CharField(max_length=50, null=True, blank=True)
