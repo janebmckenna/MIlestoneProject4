6198253d7f4c825c0c51f72c889c7ef4e37d8e83
jane mckenna <janemckenna@hotmail.com>
2024-04-20 17:26:02 +0000

    Fix: Linting home app

--- home/views.py
+++ home/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 from clubadmin.models import News
 
-# Create your views here.
+
 def index(request):
-    """ 
+    """
     A view to return an index page
     """
     news = News.objects.order_by('-date')[:10]
