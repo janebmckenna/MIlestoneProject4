f1b82b5f2511e894f1c21d3cb5effa0adb80588b
jane mckenna <janemckenna@hotmail.com>
2024-04-20 10:21:06 +0000

    Feat: added 404 and 500 pages

--- the_club/urls.py
+++ the_club/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.defaults import page_not_found, server_error
 
 
 urlpatterns = [
@@ -18,3 +19,5 @@ urlpatterns = [
     path('fixtures/', include('fixtures.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+handler404 = page_not_found
+handler500 = server_error
