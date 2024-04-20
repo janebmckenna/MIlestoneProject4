66aa3db11e38c725c056e5078ba6c456a6d82bd0
jane mckenna <janemckenna@hotmail.com>
2024-04-20 08:56:51 +0000

    Fix: added time to the fixtures model

--- fixtures/models.py
+++ fixtures/models.py
@@ -8,4 +8,5 @@ class Fixture(models.Model):
     team = models.CharField(max_length=50, null=False, blank=False)
     opponent = models.CharField(max_length=50, null=False, blank=False)
     date = models.DateField(validators=[MinValueValidator(limit_value=date.today)], null=False, blank=False)
+    time = models.CharField(max_length=50, null=False, blank=False)
     location = models.CharField(max_length=50, null=False, blank=False)
